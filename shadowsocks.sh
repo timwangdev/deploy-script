799afd3bfc50c5a8109aac82d60fb79d80024d3b
Tim Wang <timwangdev@gmail.com>
2018-08-21 11:41:47 +0800

    Create shadowsocks.sh

---
+++ shadowsocks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+
+sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
+apt update
+apt -t stretch-backports install shadowsocks-libev
